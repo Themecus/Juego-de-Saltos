53c4f65a1a56b7f76f8a2fe3d60fb23af3646981
Themecus <127447399+Themecus@users.noreply.github.com>
2025-08-06 18:10:03 -0400

    Jugador funcional, pasando a rama principal, se removió el ataque de momento, aun no es definitivo

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,12 +4,13 @@ const speed = 300.0
 const jump_velocity = -500.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var current_state:STATE=STATE.IDLE
+@onready var zone_damage=$zone_damage
 @onready var animat=$AnimatedSprite2D
 enum STATE{#con esta maquina de estados controlaremos sus acciones
 	IDLE,
 	WALK,
 	JUMP,
-	ATTACK
+	#ATTACK
 }
 
 func _physics_process(delta):
@@ -22,17 +23,17 @@ func _physics_process(delta):
 				current_state=STATE.WALK
 			elif Input.is_action_pressed("right"):#si presionamos D se activara el estado correspondiente
 				current_state=STATE.WALK
-			elif Input.is_action_pressed("attack"):#si presionamos D se activara el estado correspondiente
-				current_state=STATE.ATTACK
+			#elif Input.is_action_pressed("attack"):#si presionamos D se activara el estado correspondiente
+				#current_state=STATE.ATTACK
 			#print("quieto")
 		STATE.WALK:#aqui caminando
 			move_walk()
-			if not Input.is_action_pressed("left") and not Input.is_action_pressed("right"):#Si no se sgui presionando A o D vuelve a estar quieto
+			if not Input.is_action_pressed("left") and not Input.is_action_pressed("right") and is_on_floor():#Si no se sgui presionando A o D vuelve a estar quieto
 				current_state=STATE.IDLE
 			elif Input.is_action_pressed("jump"): #si esta caminando puede saltar
 				current_state=STATE.JUMP
-			elif Input.is_action_pressed("attack"): #si esta caminando puede saltar
-				current_state=STATE.ATTACK
+			#elif Input.is_action_pressed("attack"): #si esta caminando puede saltar
+				#current_state=STATE.ATTACK
 			#print("moverse")
 		STATE.JUMP:#aqui saltando
 			move_jump()
@@ -40,24 +41,27 @@ func _physics_process(delta):
 				current_state=STATE.IDLE
 			elif Input.is_action_pressed("left") or Input.is_action_pressed("right"): #Si esta en el aire puede moverse
 				current_state=STATE.WALK
-			elif Input.is_action_pressed("attack"): #Si esta en el aire puede moverse
-				current_state=STATE.ATTACK
+			#elif Input.is_action_pressed("attack"): #Si esta en el aire puede moverse
+				#current_state=STATE.ATTACK
 			#print("salto")
-		STATE.ATTACK:#aqui atacamos
-			move_attack()
-			if Input.is_action_pressed("right") and not Input.is_action_pressed("attack"):
-				current_state = STATE.WALK
-			if Input.is_action_pressed("left") and not Input.is_action_pressed("attack"):
-				current_state = STATE.WALK
-			elif not Input.is_action_pressed("attack"): #Si esta en el aire puede moverse
-				pass
+		#STATE.ATTACK:#aqui atacamos
+			#move_attack()
+			#if Input.is_action_pressed("right") and not Input.is_action_pressed("attack"):
+				#current_state = STATE.WALK
+			#if Input.is_action_pressed("left") and not Input.is_action_pressed("attack"):
+				#current_state = STATE.WALK
 	
 	move_gravity(delta)
 	move_and_slide()
 
 func move_jump():
 	if not is_on_floor():
+		zone_damage.monitoring = true
+		zone_damage.monitorable = true
 		animat.play("jump")
+	else:
+		zone_damage.monitoring = false
+		zone_damage.monitorable = false
 	if Input.is_action_pressed("jump") and is_on_floor():#para saltar
 		velocity.y = jump_velocity
 
@@ -67,6 +71,8 @@ func move_gravity(delta):
 
 func move_idle():
 	animat.play("idle")
+	zone_damage.monitoring = false
+	zone_damage.monitorable = false
 
 func move_stop_animat():
 	animat.pause()
@@ -84,14 +90,22 @@ func move_walk():
 	else:#esto lo que hace es que cuando dejemos de movernos limpie velocity.x con un zero y no alla residuos
 		velocity.x = move_toward(velocity.x, 0, speed)
 
-func move_attack():
-		animat.play("attack")
-		if animat.frame == animat.sprite_frames.get_frame_count("attack")-1:
-			if Input.is_action_pressed("left") or Input.is_action_pressed("right"):
-				current_state = STATE.WALK
-			else:
-				current_state = STATE.IDLE
-		else:
+func _on_body_hitbox_area_entered(area):
+	queue_free()
+
+
+#func move_attack():
+		#animat.play("attack")
+		#if animat.frame == animat.sprite_frames.get_frame_count("attack")-1:
+			
+			#if Input.is_action_pressed("left") or Input.is_action_pressed("right"):
+				#current_state = STATE.WALK
+			#else:
+				#current_state = STATE.IDLE
+		#else:
 			# Mantenerse en estado de ataque
-			velocity.x = move_toward(velocity.x, 0, speed)
+			#velocity.x = move_toward(velocity.x, 0, speed)
+
+
+
 
