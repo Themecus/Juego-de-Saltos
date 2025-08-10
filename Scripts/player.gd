3dd924e5e6d1351b1c6efaf5c88e7f3d9ad8f412
Themecus <127447399+Themecus@users.noreply.github.com>
2025-08-10 16:26:19 -0400

    Se probo el suelo y funciona los tilemap, se arreglo algunas cosas, se creo un sistema para correr y dar saltos a las paredes

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 const speed = 300.0
+const run_speed = 600.0
 const jump_velocity = -500.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var current_state:STATE=STATE.IDLE
@@ -11,6 +12,7 @@ enum STATE{#con esta maquina de estados controlaremos sus acciones
 	IDLE,
 	WALK,
 	JUMP,
+	RUN
 	#ATTACK
 }
 
@@ -24,15 +26,14 @@ func _physics_process(delta):
 				current_state=STATE.WALK
 			elif Input.is_action_pressed("right"):#si presionamos D se activara el estado correspondiente
 				current_state=STATE.WALK
-			#elif Input.is_action_pressed("attack"):#si presionamos D se activara el estado correspondiente
-				#current_state=STATE.ATTACK
-			#print("quieto")
 		STATE.WALK:#aqui caminando
 			move_walk()
 			if not Input.is_action_pressed("left") and not Input.is_action_pressed("right") and is_on_floor():#Si no se sgui presionando A o D vuelve a estar quieto
 				current_state=STATE.IDLE
 			elif Input.is_action_pressed("jump"): #si esta caminando puede saltar
 				current_state=STATE.JUMP
+			elif Input.is_action_pressed("speed"):
+				current_state=STATE.RUN
 			#elif Input.is_action_pressed("attack"): #si esta caminando puede saltar
 				#current_state=STATE.ATTACK
 			#print("moverse")
@@ -42,18 +43,41 @@ func _physics_process(delta):
 				current_state=STATE.IDLE
 			elif Input.is_action_pressed("left") or Input.is_action_pressed("right"): #Si esta en el aire puede moverse
 				current_state=STATE.WALK
-			#elif Input.is_action_pressed("attack"): #Si esta en el aire puede moverse
-				#current_state=STATE.ATTACK
-			#print("salto")
-		#STATE.ATTACK:#aqui atacamos
-			#move_attack()
-			#if Input.is_action_pressed("right") and not Input.is_action_pressed("attack"):
-				#current_state = STATE.WALK
-			#if Input.is_action_pressed("left") and not Input.is_action_pressed("attack"):
-				#current_state = STATE.WALK
+		STATE.RUN:#aqui corremos
+			move_run(delta)
+			if !Input.is_action_pressed("speed"):
+				current_state=STATE.WALK
+			elif Input.is_action_pressed("jump"):
+				current_state=STATE.JUMP
 	
 	move_gravity(delta)
 	move_and_slide()
+
+func move_run(delta):
+	if is_on_floor():
+		animat.play("walk")
+		if Input.is_action_pressed("right"):
+			animat.flip_h=false
+		if Input.is_action_pressed("left"):
+			animat.flip_h=true
+	var direction = Input.get_axis("left", "right")# con esto sabremos a que lado esta yendo
+	if direction:#dependiendo de la direccion se movera de una u otra lado
+		velocity.x = direction * run_speed
+	else:#esto lo que hace es que cuando dejemos de movernos limpie velocity.x con un zero y no alla residuos
+		velocity.x = move_toward(velocity.x, 0, run_speed)
+
+func move_walk():
+	if is_on_floor():
+		animat.play("walk")
+		if Input.is_action_pressed("right"):
+			animat.flip_h=false
+		if Input.is_action_pressed("left"):
+			animat.flip_h=true
+	var direction = Input.get_axis("left", "right")# con esto sabremos a que lado esta yendo
+	if direction:#dependiendo de la direccion se movera de una u otra lado
+		velocity.x = direction * speed
+	else:#esto lo que hace es que cuando dejemos de movernos limpie velocity.x con un zero y no alla residuos
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 func move_jump():
 	if not is_on_floor():
@@ -79,21 +103,13 @@ func move_idle():
 func move_stop_animat():
 	animat.pause()
 
-func move_walk():
-	if is_on_floor():
-		animat.play("walk")
-		if Input.is_action_pressed("right"):
-			animat.flip_h=false
-		if Input.is_action_pressed("left"):
-			animat.flip_h=true
-	var direction = Input.get_axis("left", "right")# con esto sabremos a que lado esta yendo
-	if direction:#dependiendo de la direccion se movera de una u otra lado
-		velocity.x = direction * speed
-	else:#esto lo que hace es que cuando dejemos de movernos limpie velocity.x con un zero y no alla residuos
-		velocity.x = move_toward(velocity.x, 0, speed)
-
 func _on_body_hitbox_area_entered(area):
-	queue_free()
+	if area.is_in_group("wall"):
+		double_jump=true
+	elif is_on_floor():
+		double_jump=false
+	else:
+		queue_free()
 
 func _on_zone_damage_area_entered(area):
 	double_jump=true
@@ -101,24 +117,3 @@ func _on_zone_damage_area_entered(area):
 		double_jump=false
 	if velocity.y > 0:  # Jugador moviéndose hacia abajo
 			velocity.y = -300  # Ajusta este valor para la altura del rebote
-
-
-
-#func move_attack():
-		#animat.play("attack")
-		#if animat.frame == animat.sprite_frames.get_frame_count("attack")-1:
-			
-			#if Input.is_action_pressed("left") or Input.is_action_pressed("right"):
-				#current_state = STATE.WALK
-			#else:
-				#current_state = STATE.IDLE
-		#else:
-			# Mantenerse en estado de ataque
-			#velocity.x = move_toward(velocity.x, 0, speed)
-
-
-
-
-
-
-
